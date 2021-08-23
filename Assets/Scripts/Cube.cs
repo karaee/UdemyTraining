e2fa7c336804513fda06a73bedf33d7945f68657
Emre Kara <e.kara.boun@gmail.com>
2021-08-23 18:22:32 +0300

    Project Upload

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private void Awake() {
+        Debug.Log("başla");
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("sth inside");
+    }
+}
